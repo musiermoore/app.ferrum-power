88639773fd3508a7f6dcbc0e0487eac8c0069f88
musiermoore <musiermoore@gmail.com>
2021-10-02 15:28:17 +0600

    Added new const routes

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,15 +1,41 @@
 export const ADMIN_ROUTE = '/admin'
-export const CREATE_CATEGORY_ROUTE = ADMIN_ROUTE + '/category/create'
-export const EDIT_CATEGORY_ROUTE = ADMIN_ROUTE + '/category'
-export const CREATE_PRODUCT_ROUTE = ADMIN_ROUTE + '/product/create'
-export const EDIT_PRODUCT_ROUTE = ADMIN_ROUTE + '/product'
+
+/*
+ *  Auth
+ */
 export const LOGIN_ROUTE = ADMIN_ROUTE + '/login'
 
-export const REGISTRATION_ROUTE = ADMIN_ROUTE + '/registration'
-
+/*
+ *  Categories
+ */
 export const ADMIN_CATEGORIES_LIST = ADMIN_ROUTE + '/categories'
-export const ADMIN_PRODUCT_LIST = ADMIN_ROUTE + '/products'
+export const CREATE_CATEGORY_ROUTE = ADMIN_ROUTE + '/category/create'
+export const EDIT_CATEGORY_ROUTE = ADMIN_ROUTE + '/category'
 
+/*
+ *  Products
+ */
+export const ADMIN_PRODUCT_LIST = ADMIN_ROUTE + '/products'
+export const CREATE_PRODUCT_ROUTE = ADMIN_ROUTE + '/product/create'
+export const EDIT_PRODUCT_ROUTE = ADMIN_ROUTE + '/product'
+
+/*
+ *  Users
+ */
+export const ADMIN_USERS_LIST = ADMIN_ROUTE + '/users'
+export const REGISTRATION_ROUTE = ADMIN_ROUTE + '/registration'
+export const EDIT_USER_ROUTE = ADMIN_ROUTE + '/user'
+
+/*
+ *  Orders
+ */
+export const ADMIN_ORDERS_LIST = ADMIN_ROUTE + '/orders'
+export const CREATE_ORDER_ROUTE = ADMIN_ROUTE + '/order/create'
+export const EDIT_ORDER_ROUTE = ADMIN_ROUTE + '/order'
+
+/*
+ *  Clients (Shop)
+ */
 export const SHOP_ROUTE = '/'
 export const SHOP_CATEGORY_ROUTE = '/shop/category'
 export const CART_ROUTE = '/cart'
